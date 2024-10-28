79f79de60f3278b8e3649a0345e319c8392e20b8
miguelaarrones <miguelaarrones@icloud.com>
2024-10-28 15:55:18 +0100

    Shaders!

--- BakuEngine/src/Baku/Application.cpp
+++ BakuEngine/src/Baku/Application.cpp
@@ -43,6 +43,35 @@ namespace Baku
 
         unsigned int indices[3] = { 0, 1, 2 };
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        std::string vertexSrc = R"(
+            #version 450 core
+            
+            layout(location = 0) in vec3 a_Position;
+
+            out vec3 v_Position;
+
+            void main()
+            {
+                gl_Position = vec4(a_Position, 1.0f);
+                v_Position = a_Position;
+            }
+        )";
+
+        std::string fragmentSrc = R"(
+            #version 450 core
+            
+            layout(location = 0) out vec4 color;
+
+            in vec3 v_Position;
+
+            void main()
+            {
+                color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+            }
+        )";
+
+        m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
 
     Application::~Application()
@@ -79,6 +108,7 @@ namespace Baku
             glClearColor(0.1f, 0.1f, 0.1f, 1);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            m_Shader->Bind();
             glBindVertexArray(m_VertexArray);
             glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
