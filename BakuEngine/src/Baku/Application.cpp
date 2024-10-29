66b08afbc200463067cb5a7e2ff2700080da4310
miguelaarrones <miguelaarrones@icloud.com>
2024-10-29 18:12:21 +0100

    Added buffer layout descriptors for vertex buffers

--- BakuEngine/src/Baku/Application.cpp
+++ BakuEngine/src/Baku/Application.cpp
@@ -10,6 +10,27 @@ namespace Baku
 {
     Application* Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case Baku::ShaderDataType::Float:    return GL_FLOAT;
+            case Baku::ShaderDataType::Float2:   return GL_FLOAT;
+            case Baku::ShaderDataType::Float3:   return GL_FLOAT;
+            case Baku::ShaderDataType::Float4:   return GL_FLOAT;
+            case Baku::ShaderDataType::Mat3:     return GL_FLOAT;
+            case Baku::ShaderDataType::Mat4:     return GL_FLOAT;
+            case Baku::ShaderDataType::Int:      return GL_INT;
+            case Baku::ShaderDataType::Int2:     return GL_INT;
+            case Baku::ShaderDataType::Int3:     return GL_INT;
+            case Baku::ShaderDataType::Int4:     return GL_INT;
+            case Baku::ShaderDataType::Bool:     return GL_BOOL;
+        }
+
+        BK_CORE_ASSERT(false, "Unknow ShaderDataType!");
+        return 0;
+    }
+
     Application::Application()
     {
         BK_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -24,33 +45,56 @@ namespace Baku
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
-        float vertices[3 * 3] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.0f,  0.5f, 0.0f
+        float vertices[3 * 7] = {
+            -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+             0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+             0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
         };
         
         m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-        m_VertexBuffer->Bind();
+        
+        {
+            BufferLayout layout = {
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" }
+            };
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+            m_VertexBuffer->SetLayout(layout);
+        }
+
+        uint32_t index = 0;
+        const auto& layout = m_VertexBuffer->GetLayout();
+        for (auto& element : layout)
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                index, 
+                element.GetComponentCount(),
+                ShaderDataTypeToOpenGLBaseType(element.Type), 
+                element.Normalized ? GL_TRUE : GL_FALSE,
+                layout.GetStride(),
+                (const void*)element.Offset
+            );
+            index++;
+        }
 
         uint32_t indices[3] = { 0, 1, 2 };
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-        m_IndexBuffer->Bind();
 
         std::string vertexSrc = R"(
             #version 450 core
             
             layout(location = 0) in vec3 a_Position;
+            layout(location = 1) in vec4 a_Color;
 
             out vec3 v_Position;
+            out vec4 v_Color;
 
             void main()
             {
                 gl_Position = vec4(a_Position, 1.0f);
                 v_Position = a_Position;
+                v_Color = a_Color;
             }
         )";
 
@@ -60,10 +104,12 @@ namespace Baku
             layout(location = 0) out vec4 color;
 
             in vec3 v_Position;
+            in vec4 v_Color;
 
             void main()
             {
                 color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+                color = v_Color;
             }
         )";
 
