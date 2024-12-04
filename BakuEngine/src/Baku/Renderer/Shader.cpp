46f861efa38cf1582c97dae7b535562c93f5f7d7
miguelaarrones <miguelaarrones@icloud.com>
2024-12-04 20:13:26 +0100

    Shaders can be read from files

--- BakuEngine/src/Baku/Renderer/Shader.cpp
+++ BakuEngine/src/Baku/Renderer/Shader.cpp
@@ -6,6 +6,21 @@
 
 namespace Baku
 {
+    Shader* Shader::Create(const std::string& filePath)
+    {
+        switch (Renderer::GetAPI())
+        {
+            case RendererAPI::API::None:
+                BK_CORE_ASSERT(false, "RendererAPI::None is currently not supported!")
+                    return nullptr;
+            case RendererAPI::API::OpenGL:
+                return new OpenGLShader(filePath);
+        }
+
+        BK_CORE_ASSERT(false, "Unknown RendererAPI!")
+            return nullptr;
+    }
+
     Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
     {
         switch (Renderer::GetAPI())
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -133,40 +133,7 @@ public:
 
         m_FlatColorShader.reset(Baku::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
-        std::string textureShaderVertexSrc = R"(
-            #version 450 core
-            
-            layout(location = 0) in vec3 a_Position;
-            layout(location = 1) in vec2 a_TexCoord;
-
-            uniform mat4 u_ViewProjection;
-            uniform mat4 u_Transform;
-
-            out vec2 v_TexCoord;
-
-            void main()
-            {
-                v_TexCoord = a_TexCoord;
-                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0f);
-            }
-        )";
-
-        std::string textureColorShaderFragmentSrc = R"(
-            #version 450 core
-            
-            layout(location = 0) out vec4 color;
-
-            in vec2 v_TexCoord;
-
-            uniform sampler2D u_Texture;
-
-            void main()
-            {
-                color = texture(u_Texture, v_TexCoord);
-            }
-        )";
-
-        m_TextureShader.reset(Baku::Shader::Create(textureShaderVertexSrc, textureColorShaderFragmentSrc));
+        m_TextureShader.reset(Baku::Shader::Create("assets/shaders/Texture.glsl"));
 
         m_Texture = Baku::Texture2D::Create("assets/textures/Checkerboard.png");
         m_LogoTexture = Baku::Texture2D::Create("assets/textures/Logo.png");
