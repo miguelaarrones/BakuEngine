b221610619e814800626b33c2e3e98d340028c41
miguelaarrones <miguelaarrones@icloud.com>
2024-12-05 21:51:09 +0100

    Added proper window resize handling

--- BakuEngine/src/Baku/Application.cpp
+++ BakuEngine/src/Baku/Application.cpp
@@ -39,6 +39,7 @@ namespace Baku
     {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(BK_BIND_EVENT_FN(Application::OnWindowClose));
+        dispatcher.Dispatch<WindowResizeEvent>(BK_BIND_EVENT_FN(Application::OnWindowResize));
 
         for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
         {
@@ -56,8 +57,11 @@ namespace Baku
             Timestep timestep = time - m_LastFrameTime;
             m_LastFrameTime = time;
 
-            for (Layer* layer : m_LayerStack)
-                layer->OnUpdate(timestep);
+            if (!m_Minimized)
+            {
+                for (Layer* layer : m_LayerStack)
+                    layer->OnUpdate(timestep);
+            }
 
             m_ImGuiLayer->Begin();
             for (Layer* layer : m_LayerStack)
@@ -68,10 +72,23 @@ namespace Baku
         }
     }
 
-
     bool Application::OnWindowClose(WindowCloseEvent& e)
     {
         m_Running = false;
         return true;
+    }
+
+    bool Application::OnWindowResize(WindowResizeEvent& e)
+    {
+        if (e.GetWidth() == 0 || e.GetHeight() == 0)
+        {
+            m_Minimized = true;
+            return false;
+        }
+
+        m_Minimized = false;
+        Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+        return false;
     }
 }
