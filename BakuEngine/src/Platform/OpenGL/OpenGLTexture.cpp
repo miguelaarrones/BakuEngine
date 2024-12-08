b967900498dae27b33d03371752df0aaf658574a
miguelaarrones <miguelaarrones@icloud.com>
2024-12-08 20:02:53 +0100

    Using only one Shader for the textured and non-textured quads

--- BakuEngine/src/Platform/OpenGL/OpenGLTexture.cpp
+++ BakuEngine/src/Platform/OpenGL/OpenGLTexture.cpp
@@ -2,10 +2,25 @@
 #include "OpenGLTexture.h"
 
 #include "stb_image.h"
-#include <glad/glad.h>
 
 namespace Baku
 {
+    OpenGLTexture2D::OpenGLTexture2D(uint32_t width, uint32_t height)
+        : m_Width(width), m_Height(height)
+    {
+        m_InternalFormat = GL_RGBA8;
+        m_DataFormat = GL_RGBA;
+
+        glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
+        glTextureStorage2D(m_RendererID, 1, m_InternalFormat, m_Width, m_Height);
+
+        glTextureParameteri(m_RendererID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTextureParameteri(m_RendererID, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    }
+
     OpenGLTexture2D::OpenGLTexture2D(const std::string& path)
         : m_Path(path)
     {
@@ -27,6 +42,9 @@ namespace Baku
             dataFormat = GL_RGB;
         }
 
+        m_InternalFormat = internalFormat;
+        m_DataFormat = dataFormat;
+
         BK_CORE_ASSERT(internalFormat & dataFormat, "Image format not supported!");
 
         glCreateTextures(GL_TEXTURE_2D, 1, &m_RendererID);
@@ -46,6 +64,13 @@ namespace Baku
     OpenGLTexture2D::~OpenGLTexture2D()
     {
         glDeleteTextures(1, &m_RendererID);
+    }
+
+    void OpenGLTexture2D::SetData(void* data, uint32_t size)
+    {
+        uint32_t bytesPerPixel = m_DataFormat == GL_RGBA ? 4 : 3;
+        BK_ASSERT(size == m_Width * m_Height * bytesPerPixel, "Data must be entire texture!");
+        glTextureSubImage2D(m_RendererID, 0, 0, 0, m_Width, m_Height, m_DataFormat, GL_UNSIGNED_BYTE, data);
     }
 
     void OpenGLTexture2D::Bind(uint32_t slot) const
