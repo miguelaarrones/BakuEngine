e57889b2da1e0ebfe1b22e69fa757378460561bd
miguelaarrones <miguelaarrones@icloud.com>
2024-12-11 19:02:02 +0100

    Added textures to batch renderer

--- BakuEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ BakuEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -212,6 +212,13 @@ namespace Baku
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string name, int* values, uint32_t count)
+	{
+		BK_PROFILE_FUNCTION();
+
+		UploadUniformIntArray(name, values, count);
+	}
+
 	void OpenGLShader::SetFloat(const std::string name, const float value)
 	{
 		BK_PROFILE_FUNCTION();
@@ -244,6 +251,12 @@ namespace Baku
 	{
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
