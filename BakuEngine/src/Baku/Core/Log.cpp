325a9f22ed98562674119de47567558c41bae55d
miguelaarrones <miguelaarrones@icloud.com>
2024-12-11 11:46:09 +0100

    Log messages are now additionally sent to Baku.log file
    - Changed to use sinks
    - Just one console sink
    - One sink and one log file only, different patterns
    - Changed pattern
    - Added *.log and BakuProfile-*.json to .gitignore
    - Added timestamps to the log file and set flush level.

--- BakuEngine/src/Baku/Core/Log.cpp
+++ BakuEngine/src/Baku/Core/Log.cpp
@@ -2,6 +2,7 @@
 #include "Baku/Core/Log.h"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Baku {
 
@@ -11,12 +12,22 @@ namespace Baku {
     void Log::Init() 
     {
         // https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
-        spdlog::set_pattern("%^[%T] %n: %v%$");
+        
+        std::vector<spdlog::sink_ptr> logSinks;
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+        logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Baku.log", true));
+        logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+        logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+        s_CoreLogger = std::make_shared<spdlog::logger>("BAKU", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_CoreLogger);
 
-        s_CoreLogger = spdlog::stdout_color_mt("BAKU");
         s_CoreLogger->set_level(spdlog::level::trace);
+        s_CoreLogger->flush_on(spdlog::level::trace);
 
-        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+        spdlog::register_logger(s_ClientLogger);
+
         s_ClientLogger->set_level(spdlog::level::trace);
+        s_ClientLogger->flush_on(spdlog::level::trace);
     }
 }
