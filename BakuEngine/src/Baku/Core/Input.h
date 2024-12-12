387d3777206bf2fa579633c4bbe417d255aefa7f
miguelaarrones <miguelaarrones@icloud.com>
2024-12-12 11:45:37 +0100

    Rendering rotated quads in batches and using transforms now
    Also added to the commit a few files that did not got saved last commit.

--- BakuEngine/src/Baku/Core/Input.h
+++ BakuEngine/src/Baku/Core/Input.h
@@ -14,12 +14,12 @@ namespace Baku
         Input(const Input&) = delete;
         Input& operator=(const Input&) = delete;
 
-        inline static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
+        static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
         
-        inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-        inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-        inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-        inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
+        static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
+        static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
+        static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
+        static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
 
         static Scope<Input> Create();
     protected:
--- BakuEngine/src/Baku/Events/ApplicationEvent.h
+++ BakuEngine/src/Baku/Events/ApplicationEvent.h
@@ -12,8 +12,8 @@ namespace Baku
             : m_Width(width), m_Height(height)
         {}
 
-        inline unsigned int GetWidth() const { return m_Width; }
-        inline unsigned int GetHeight() const { return m_Height; }
+        unsigned int GetWidth() const { return m_Width; }
+        unsigned int GetHeight() const { return m_Height; }
 
         std::string ToString() const override
         {
