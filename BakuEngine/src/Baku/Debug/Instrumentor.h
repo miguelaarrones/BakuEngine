702181b65bd6ed706e13b1320e2b2d162df9d15d
miguelaarrones <miguelaarrones@icloud.com>
2024-12-12 17:02:10 +0100

    Optimization in Instrumentor.h
    - Remove __cdecl from scope name at compile time
    - Replace " with ' in scope name at compile time

--- BakuEngine/src/Baku/Debug/Instrumentor.h
+++ BakuEngine/src/Baku/Debug/Instrumentor.h
@@ -78,13 +78,10 @@ namespace Baku
         {
             std::stringstream json;
 
-            std::string name = result.Name;
-            std::replace(name.begin(), name.end(), '"', '\'');
-
             json << ",{";
             json << "\"cat\":\"function\",";
             json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-            json << "\"name\":\"" << name << "\",";
+            json << "\"name\":\"" << result.Name << "\",";
             json << "\"ph\":\"X\",";
             json << "\"pid\":0,";
             json << "\"tid\":" << result.ThreadID << ",";
@@ -160,6 +157,33 @@ namespace Baku
         std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
         bool m_Stopped;
     };
+    
+    namespace InstrumentorUtils
+    {
+        template <size_t N>
+        struct ChangeResult
+        {
+            char Data[N];
+        };
+        template <size_t N, size_t K>
+        constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+        {
+            ChangeResult<N> result = {};
+            size_t srcIndex = 0;
+            size_t dstIndex = 0;
+            while (srcIndex < N)
+            {
+                size_t matchIndex = 0;
+                while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+                    matchIndex++;
+                if (matchIndex == K - 1)
+                    srcIndex += matchIndex;
+                result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+                srcIndex++;
+            }
+            return result;
+        }
+    }
 }
 
 #define BK_PROFILE 1
@@ -171,7 +195,7 @@ namespace Baku
     #define BK_FUNC_SIG __PRETTY_FUNCTION__
     #elif defined(__DMC__) && (__DMC__ >= 0x810)
     #define BK_FUNC_SIG __PRETTY_FUNCTION__
-    #elif defined(__FUNCSIG__)
+    #elif (defined(__FUNCSIG__) || (_MSC_VER))
     #define BK_FUNC_SIG __FUNCSIG__
     #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
     #define BK_FUNC_SIG __FUNCTION__
@@ -187,7 +211,8 @@ namespace Baku
 
     #define BK_PROFILE_BEGIN_SESSION(name, filePath) ::Baku::Instrumentor::Get().BeginSession(name, filePath)
     #define BK_PROFILE_END_SESSION() ::Baku::Instrumentor::Get().EndSession()
-    #define BK_PROFILE_SCOPE(name) ::Baku::InstrumentationTimer timer##__LINE__(name);
+    #define BK_PROFILE_SCOPE(name) constexpr auto fixedName = ::Baku::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+									                                ::Baku::InstrumentationTimer timer##__LINE__(fixedName.Data)
     #define BK_PROFILE_FUNCTION() BK_PROFILE_SCOPE(BK_FUNC_SIG)
 #else
     #define BK_PROFILE_BEGIN_SESSION(name, filePath)
