3645a3dfb9bfc4ea0570aecc8a3838a483e79a08
miguelaarrones <miguelaarrones@icloud.com>
2024-12-18 10:49:59 +0100

    Implemented viewport in BakuEditor

--- BakuEngine/src/Baku/OrthographicCameraController.cpp
+++ BakuEngine/src/Baku/OrthographicCameraController.cpp
@@ -66,6 +66,12 @@ namespace Baku
 		dispatcher.Dispatch<WindowResizeEvent>(BK_BIND_EVENT_FN(OrthographicCameraController::OnWindowResized));
 	}
 
+	void OrthographicCameraController::OnResize(float width, float height)
+	{
+		m_AspectRatio = width / height;
+		CalculateView();
+	}
+
 	void OrthographicCameraController::CalculateView()
 	{
 		m_Bounds = { -m_AspectRatio * m_ZoomLevel, m_AspectRatio * m_ZoomLevel, -m_ZoomLevel, m_ZoomLevel };
--- BakuEngine/src/Baku/OrthographicCameraController.h
+++ BakuEngine/src/Baku/OrthographicCameraController.h
@@ -27,6 +27,8 @@ namespace Baku
 		void OnUpdate(Timestep ts);
 		void OnEvent(Event& e);
 
+		void OnResize(float width, float height);
+
 		OrthographicCamera& GetCamera() { return m_Camera; }
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
 
