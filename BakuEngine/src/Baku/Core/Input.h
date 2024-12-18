4d614dfa34fb5e493e29d2cb2a9a56b3c7dbf891
miguelaarrones <miguelaarrones@icloud.com>
2024-12-18 11:16:33 +0100

    Input refactor

--- BakuEngine/src/Baku/Core/Input.h
+++ BakuEngine/src/Baku/Core/Input.h
@@ -8,30 +8,12 @@ namespace Baku
 {
     class Input
     {
-    protected:
-        Input() = default;
     public:
-        virtual ~Input() = default;
-        
-        Input(const Input&) = delete;
-        Input& operator=(const Input&) = delete;
+        static bool IsKeyPressed(KeyCode key);
 
-        static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }
-        
-        static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-        static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-        static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-        static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-
-        static Scope<Input> Create();
-    protected:
-        virtual bool IsKeyPressedImpl(KeyCode  key) = 0;
-        virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-        virtual float GetMouseXImpl() = 0;
-        virtual float GetMouseYImpl() = 0;
-        virtual std::pair<float, float> GetMousePositionImpl() = 0;
-
-    private:
-        static Scope<Input> s_Instance;
+        static bool IsMouseButtonPressed(MouseCode button);
+        static std::pair<float, float> GetMousePosition();
+        static float GetMouseX();
+        static float GetMouseY();
     };
 }
--- BakuEngine/src/Platform/Windows/WindowsInput.cpp
+++ BakuEngine/src/Platform/Windows/WindowsInput.cpp
@@ -1,5 +1,5 @@
 #include "bkpch.h"
-#include "Platform/Windows/WindowsInput.h"
+#include "Baku/Core/Input.h"
 
 #include "Baku/Core/Application.h"
 
@@ -7,7 +7,7 @@
 
 namespace Baku
 {
-    bool WindowsInput::IsKeyPressedImpl(KeyCode keyCode)
+    bool Input::IsKeyPressed(KeyCode keyCode)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetKey(window, static_cast<int32_t>(keyCode));
@@ -15,7 +15,7 @@ namespace Baku
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
-    bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+    bool Input::IsMouseButtonPressed(MouseCode button)
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
@@ -23,24 +23,24 @@ namespace Baku
         return state == GLFW_PRESS;
     }
 
-    float WindowsInput::GetMouseXImpl()
-    {
-        auto [x, y] = GetMousePosition();
-        return x;
-    }
-
-    float WindowsInput::GetMouseYImpl()
-    {
-        auto [x, y] = GetMousePosition();
-        return y;
-    }
-
-    std::pair<float, float> WindowsInput::GetMousePositionImpl()
+    std::pair<float, float> Input::GetMousePosition()
     {
         auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
         double xPos, yPos;
         glfwGetCursorPos(window, &xPos, &yPos);
 
         return { (float)xPos, (float)yPos };
+    }
+
+    float Input::GetMouseX()
+    {
+        auto [x, y] = GetMousePosition();
+        return x;
+    }
+
+    float Input::GetMouseY()
+    {
+        auto [x, y] = GetMousePosition();
+        return y;
     }
 }
